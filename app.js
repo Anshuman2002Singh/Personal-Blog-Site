da59881ef39cf0758ec0471eea53ce0e274a1559
Anshu <anshul.ansh.as@gmail.com>
2023-07-27 23:56:26 +0530

    nth commit

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ app.get("/contact",function(req,res){                                           
 app.get("/compose",function(req,res){                                            // rendering contact page through contact.ejs
   res.render("compose");
 })
+app.post("/compose",function(req,res){
+  const post={
+    title:req.body.postTitle,
+    content:req.body.postBody
+    
+  };
+  console.log(post);
+})
 
 
 
